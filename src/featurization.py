83497289fd8670129450dfd076f2ddb8ca845242
vishal <vishalbollu@users.noreply.github.com>
2019-11-07 15:26:03 -0500

    Export pipeline pickle in featurization step

--- src/featurization.py
+++ src/featurization.py
@@ -5,6 +5,7 @@ import pandas as pd
 import numpy as np
 import scipy.sparse as sparse
 
+from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 
@@ -15,19 +16,21 @@ except ImportError:
 
 np.set_printoptions(suppress=True)
 
-if len(sys.argv) != 3 and len(sys.argv) != 5:
-    sys.stderr.write('Arguments error. Usage:\n')
-    sys.stderr.write('\tpython featurization.py data-dir-path features-dir-path\n')
+if len(sys.argv) != 4:
+    sys.stderr.write("Arguments error. Usage:\n")
+    sys.stderr.write(
+        "\tpython featurization.py data-dir-path features-dir-path pipeline-pickle-path\n"
+    )
     sys.exit(1)
 
-train_input = os.path.join(sys.argv[1], 'train.tsv')
-test_input = os.path.join(sys.argv[1], 'test.tsv')
-train_output = os.path.join(sys.argv[2], 'train.pkl')
-test_output = os.path.join(sys.argv[2], 'test.pkl')
+train_input = os.path.join(sys.argv[1], "train.tsv")
+test_input = os.path.join(sys.argv[1], "test.tsv")
+train_output = os.path.join(sys.argv[2], "train.pkl")
+test_output = os.path.join(sys.argv[2], "test.pkl")
 
 try:
     reload(sys)
-    sys.setdefaultencoding('utf-8')
+    sys.setdefaultencoding("utf-8")
 except NameError:
     pass
 
@@ -44,13 +47,9 @@ def mkdir_p(path):
 
 def get_df(data):
     df = pd.read_csv(
-        data,
-        encoding='utf-8',
-        header=None,
-        delimiter='\t',
-        names=['id', 'label', 'text']
+        data, encoding="utf-8", header=None, delimiter="\t", names=["id", "label", "text"]
     )
-    sys.stderr.write('The input data frame {} size is {}\n'.format(data, df.shape))
+    sys.stderr.write("The input data frame {} size is {}\n".format(data, df.shape))
     return df
 
 
@@ -58,12 +57,12 @@ def save_matrix(df, matrix, output):
     id_matrix = sparse.csr_matrix(df.id.astype(np.int64)).T
     label_matrix = sparse.csr_matrix(df.label.astype(np.int64)).T
 
-    result = sparse.hstack([id_matrix, label_matrix, matrix], format='csr')
+    result = sparse.hstack([id_matrix, label_matrix, matrix], format="csr")
 
-    msg = 'The output matrix {} size is {} and data type is {}\n'
+    msg = "The output matrix {} size is {} and data type is {}\n"
     sys.stderr.write(msg.format(output, result.shape, result.dtype))
 
-    with open(output, 'wb') as fd:
+    with open(output, "wb") as fd:
         pickle.dump(result, fd, pickle.HIGHEST_PROTOCOL)
     pass
 
@@ -72,23 +71,23 @@ mkdir_p(sys.argv[2])
 
 # Generate train feature matrix
 df_train = get_df(train_input)
-train_words = np.array(df_train.text.str.lower().values.astype('U'))
+train_words = np.array(df_train.text.str.lower().values.astype("U"))
 
-bag_of_words = CountVectorizer(stop_words='english',
-                               max_features=6000, ngram_range=(1, 2))
-bag_of_words.fit(train_words)
-train_words_binary_matrix = bag_of_words.transform(train_words)
+bag_of_words = CountVectorizer(stop_words="english", max_features=6000, ngram_range=(1, 2))
 tfidf = TfidfTransformer(smooth_idf=False)
-tfidf.fit(train_words_binary_matrix)
-train_words_tfidf_matrix = tfidf.transform(train_words_binary_matrix)
+pipeline = Pipeline(steps=[("bag_of_words", bag_of_words), ("tfidf", tfidf)])
+
+train_words_tfidf_matrix = pipeline.fit_transform(train_words)
 
 save_matrix(df_train, train_words_tfidf_matrix, train_output)
 
 # Generate test feature matrix
 df_test = get_df(test_input)
-test_words = np.array(df_test.text.str.lower().values.astype('U'))
-test_words_binary_matrix = bag_of_words.transform(test_words)
-test_words_tfidf_matrix = tfidf.transform(test_words_binary_matrix)
+test_words = np.array(df_test.text.str.lower().values.astype("U"))
+test_words_tfidf_matrix = pipeline.transform(test_words)
 
 save_matrix(df_test, test_words_tfidf_matrix, test_output)
 
+# Export pipeline pickle
+with open(sys.argv[3], "wb") as fd:
+    pickle.dump(pipeline, fd, pickle.HIGHEST_PROTOCOL)
